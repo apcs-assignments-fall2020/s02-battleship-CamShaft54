9512a98c644ad465fba644173e5481fc7c0935ec
camshaft54 <cam.shaw1234@gmail.com>
2021-01-20 09:01:21 -0800

    Complete Battleship method

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,9 +2,14 @@ public class MyMain {
     // This method returns true/false if there 
     // was a boat the specified coordinates. This
     // method also prints out an appropriate message
-    public static boolean hit(boolean[][] board, int row, int col) { 
-        // YOUR CODE HERE
-        return false;
+    public static boolean hit(boolean[][] board, int row, int col) {
+        if (board[row][col]) {
+            System.out.println("There was a hit!");
+            return true;
+        } else {
+            System.out.println("You missed a boat!");
+            return false;
+        }
     }
 
 
@@ -13,7 +18,15 @@ public class MyMain {
     // The remaining pieces are placed in the direction given
     // by the direction input
     public static boolean[][] placeBoat(boolean[][] board, String direction, int boatLength, int row, int col) { 
-        // YOUR CODE HERE
+        if (direction.equals("down")) {
+            for (int i = 0; i < boatLength; i++) {
+                board[row+i][col] = true;
+            }
+        } else {
+            for (int j = 0; j < boatLength; j++) {
+                board[row][col+j] = true;
+            }
+        }
         return board;
     }
 
@@ -22,8 +35,19 @@ public class MyMain {
     // increasing length
     // You may assume that all Strings are lowercase 
     public static boolean inOrder(String[][] words) { 
-        // YOUR CODE HERE
-        return false;
+        for (int row = 0; row < words.length; row++) {
+            String previousWord = "";
+            for (int col = 0; col < words[0].length; col++) {
+                if (previousWord.length() == 0) {
+                    previousWord = words[row][col];
+                } else if ((int) words[row][col].charAt(0) > previousWord.charAt(0) && words[row][col].length() > previousWord.length()) {
+                    previousWord = words[row][col];
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
